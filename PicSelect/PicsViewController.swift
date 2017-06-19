36dd9f136bf21d1c962eedf69bf67e7c31c392fb
Bhimasena Patri <himasena.rao@gmail.com>
2017-06-19 22:21:22 +0800

    Image addition is working, but saving is yet to be added

--- PicSelect/PicsViewController.swift
+++ PicSelect/PicsViewController.swift
@@ -15,7 +15,7 @@ class PicsViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     @IBOutlet weak var titleTextField: UITextField!
     
-    var imagePicker = UIImagePickerController()
+    var imagePickerPic = UIImagePickerController()
     
     
     
@@ -23,7 +23,7 @@ class PicsViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        imagePicker.delegate = self
+        imagePickerPic.delegate = self
         
     }
 
@@ -34,11 +34,14 @@ class PicsViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     @IBAction func photoLibraryTapped(_ sender: Any) {
         
-        imagePicker.sourceType = .photoLibrary
-        present(imagePicker, animated: true, completion: nil)
-        
+        imagePickerPic.sourceType = .photoLibrary
+        present(imagePickerPic, animated: true, completion: nil)
     }
-    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+        imagePickerPic.dismiss(animated: true, completion: nil)
+    }
     
     @IBAction func addTapped(_ sender: Any) {
     }
